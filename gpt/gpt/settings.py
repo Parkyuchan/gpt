972cdbe359aec5de75aeb3386c7a7a76713ca8a4
박유찬 <109130875+Parkyuchan@users.noreply.github.com>
2024-05-23 19:34:50 +0900

    Update settings.py

--- gpt/gpt/settings.py
+++ gpt/gpt/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q77bfl0x%%@ri&ean+vw0-j*x+5w6z$w(wc_19saeuk)^x)x3t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
